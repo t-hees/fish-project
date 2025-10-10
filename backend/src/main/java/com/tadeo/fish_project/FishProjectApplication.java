dc8af87abc680c1211ad3ea035b7aadb5c1300a1
Tadeo Volker Hees <tadeohees@gmail.com>
2025-10-11 00:22:50 +0200

    Init project

---
+++ backend/src/main/java/com/tadeo/fish_project/FishProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tadeo.fish_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishProjectApplication.class, args);
+	}
+
+}
